c8dde488477431a23ed8b37a5440e9b3a833cad7
정삼복 <68502584+3boku@users.noreply.github.com>
2024-08-13 23:08:59 +0900

    toml 파일 생성

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,11 +1,28 @@
 package cmd
 
-import "os"
+import (
+	"fmt"
+	"github.com/naoina/toml"
+	"os"
+)
+
+type Config struct {
+	Server struct {
+		Port string
+	}
+	MySQL struct {
+		Host string
+	}
+}
 
 func NewCmd(filePath string) {
+	c := new(Config)
 	if file, err := os.Open(filePath); err != nil {
 		panic(err)
 	} else if err := toml.NewDecoder(file).Decode(c); err != nil {
 		panic(err)
 	}
+
+	fmt.Println(c.MySQL.Host)
+	fmt.Println(c.Server.Port)
 }
--- init/main.go
+++ init/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"Quizep_server/init/cmd"
+	"flag"
 )
 
+var configPathFlag = flag.String("config", "config.toml", "config file not found")
+
 func main() {
-	cmd.NewCmd()
+	flag.Parse()
+	cmd.NewCmd(*configPathFlag)
 }
