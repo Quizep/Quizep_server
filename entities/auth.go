1d4024b5c5225934af202775d6357efccbace8d9
정삼복 <68502584+3boku@users.noreply.github.com>
2024-08-13 23:48:38 +0900

    유저 Entity 작성

--- entities/auth.go
+++ entities/auth.go
@@ -1,1 +1,39 @@
 package entities
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"gorm.io/gorm"
+)
+
+type StringSlice []string
+
+func (ss *StringSlice) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("failed to unmarshal StringSlice value")
+	}
+
+	return json.Unmarshal(bytes, ss)
+}
+
+func (ss StringSlice) Value() (driver.Value, error) {
+	return json.Marshal(ss)
+}
+
+type UserDAO struct {
+	gorm.Model
+	UUID       string `gorm:"primary_key"`
+	Email      string `gorm:"unique"`
+	Nigganame  string
+	Sex        string
+	CreateExam StringSlice
+	SolvedExam StringSlice
+}
+
+type UserDTO struct {
+	Email     string `json:"email"`
+	Nigganame string `json:"nigganame"`
+	Sex       string `json:"sex"`
+}
