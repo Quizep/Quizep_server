4f2785491595a229f45aee3586264c7e0fda765a
정삼복 <68502584+3boku@users.noreply.github.com>
2024-08-13 23:04:09 +0900

    yaml파일 세팅

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,5 +1,11 @@
 package cmd
 
-func NewCmd() {
+import "os"
 
+func NewCmd(filePath string) {
+	if file, err := os.Open(filePath); err != nil {
+		panic(err)
+	} else if err := toml.NewDecoder(file).Decode(c); err != nil {
+		panic(err)
+	}
 }
