88d695c496cf91830681a93a96e718987ac75dd5
정삼복 <68502584+3boku@users.noreply.github.com>
2024-08-13 23:50:18 +0900

    테이블 생성

--- reposistories/MySQL.go
+++ reposistories/MySQL.go
@@ -1,6 +1,7 @@
 package reposistories
 
 import (
+	"Quizep_server/entities"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,5 +9,6 @@ import (
 func NewMySQL(dns string) (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 
+	err = db.AutoMigrate(&entities.UserDAO{})
 	return db, err
 }
